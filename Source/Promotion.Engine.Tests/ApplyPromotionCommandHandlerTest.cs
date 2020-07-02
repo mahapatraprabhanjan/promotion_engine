29d1d42b0e39a6b77a646f4bb1cb4069f2bbf7d8
Prabhanjan Kumar Mahapatra <mahapatra.prabhanjan@hotmail.com>
2020-07-02 21:59:46 +0530

    Unit test Source created

---
+++ Source/Promotion.Engine.Tests/ApplyPromotionCommandHandlerTest.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using Promotion.Engine.API.Application.Commands;
+using System.Collections.Generic;
+
+namespace Promotion.Engine.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCaseSource("_sourceLists")]
+        public void HandleApplyPromotion(List<ApplyPromotionCommand> cart, int total)
+        {
+            Assert.Pass();
+        }
+
+        private static readonly object[] _sourceLists =
+        {
+            new object[] { new List<ApplyPromotionCommand>
+            {
+                new ApplyPromotionCommand("A", 1),
+                new ApplyPromotionCommand("B", 1),
+                new ApplyPromotionCommand("C", 1)
+            }, 100},
+            new object[] { new List<ApplyPromotionCommand>
+            {
+                new ApplyPromotionCommand("A", 5),
+                new ApplyPromotionCommand("B", 5),
+                new ApplyPromotionCommand("C", 1)
+            }, 370},
+            new object[] { new List<ApplyPromotionCommand>
+            {
+                new ApplyPromotionCommand("A", 3),
+                new ApplyPromotionCommand("B", 5),
+                new ApplyPromotionCommand("C", 1),
+                new ApplyPromotionCommand("D", 1)
+            }, 280}
+        };
+    }
+}
---
+++ Source/Services/Promotion.Engine.API/Application/Commands/ApplyPromotionCommand.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Promotion.Engine.API.Application.Commands
+{
+    public class ApplyPromotionCommand
+    {
+        [JsonProperty]
+        public string SKU { get; private set; }
+
+        [JsonProperty]
+        public int Quantity { get; private set; }
+
+        [JsonConstructor]
+        public ApplyPromotionCommand(string sku, int quantity)
+        {
+            SKU = sku;
+            Quantity = quantity;
+        }
+    }
+}
